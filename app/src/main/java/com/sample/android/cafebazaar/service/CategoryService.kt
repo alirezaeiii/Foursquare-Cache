440af8a6f6f99b92d0ebe08166941b6c30f617ac
Ali Rezaei <ali.rezaei09@gmail.com>
2020-05-16 01:10:02 +0430

    Context.startForegroundService() did not then call Service.startForeground()

--- app/src/main/java/com/sample/android/cafebazaar/service/CategoryService.kt
+++ app/src/main/java/com/sample/android/cafebazaar/service/CategoryService.kt
@@ -1,18 +1,24 @@
 package com.sample.android.cafebazaar.service
 
 import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 import com.sample.android.cafebazaar.repository.CategoryRepository
 import com.sample.android.cafebazaar.util.isNetworkAvailable
 import dagger.android.DaggerService
 import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
+
 
 class CategoryService : DaggerService() {
 
@@ -46,6 +52,14 @@ class CategoryService : DaggerService() {
         super.onCreate()
         locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000L, 1000f, locationListener)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "channelId"
+            val channel = NotificationChannel(channelId, "Update location", NotificationManager.IMPORTANCE_LOW)
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(channel)
+            val notification: Notification = NotificationCompat.Builder(this, channelId)
+                .setContentTitle("").setContentText("").build()
+            startForeground(1, notification)
+        }
     }
 
     override fun onDestroy() {
