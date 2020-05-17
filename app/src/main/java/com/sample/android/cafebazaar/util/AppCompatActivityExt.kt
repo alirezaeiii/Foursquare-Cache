1630964c41463bae80f8432a9aae3f5adc902575
Ali Rezaei <ali.rezaei09@gmail.com>
2020-05-17 22:27:35 +0430

    Remove unused code

--- app/src/main/java/com/sample/android/cafebazaar/util/AppCompatActivityExt.kt
+++ app/src/main/java/com/sample/android/cafebazaar/util/AppCompatActivityExt.kt
@@ -3,32 +3,10 @@ package com.sample.android.cafebazaar.util
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 
 fun AppCompatActivity.setupActionBar(toolbar : Toolbar, action: ActionBar.() -> Unit) {
     setSupportActionBar(toolbar)
     supportActionBar?.run {
         action()
     }
-}
-
-/**
- * The `fragment` is added to the container view with id `frameId`. The operation is
- * performed by the `fragmentManager`.
- */
-fun AppCompatActivity.addFragmentToActivity(fragment: Fragment, frameId: Int) {
-    supportFragmentManager.transact {
-        add(frameId, fragment)
-    }
-}
-
-/**
- * Runs a FragmentTransaction, then calls commit().
- */
-private inline fun FragmentManager.transact(action: FragmentTransaction.() -> Unit) {
-    beginTransaction().apply {
-        action()
-    }.commit()
 }
