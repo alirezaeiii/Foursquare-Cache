93ae8cbc5ea267e0d057e77ea738b115e7dbd858
Ali Rezaei <ali.rezaei09@gmail.com>
2021-03-08 19:44:18 +0330

    Modification

--- app/src/main/java/com/sample/android/cafebazaar/FoursquareApplication.kt
+++ app/src/main/java/com/sample/android/cafebazaar/FoursquareApplication.kt
@@ -13,7 +13,7 @@ class FoursquareApplication : DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
 
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
